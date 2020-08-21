ba76d58011523a14e402f104507828f254ed9213
Vim-o <suktaej@gmail.com>
2020-08-21 22:00:19 +0900

    Q1'

---
+++ hello_test/max_test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int max4(int *a,int *b,int *c,int *d);
+
+int main(void){
+
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    cout<<max4(&a,&b,&c,&d)<<endl;
+
+    return 0;
+} 
+
+int max4(int *a,int *b,int *c,int *d){
+
+    int max_num=*a;
+
+    if(*b>max_num) max_num=*b;
+    if(*c>max_num) max_num=*c;
+    if(*d>max_num) max_num=*d;
+
+    return max_num;
+}

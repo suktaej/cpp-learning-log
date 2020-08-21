61c4847be873356ddedc1645a6dae4c28f7223dd
Vim-o <suktaej@gmail.com>
2020-08-21 22:27:55 +0900

    commit test2

--- max_test.cpp
+++ max_test.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int max4(int *a,int *b,int *c,int *d);
+int max4(int *a,int *b,int *c,int *d){
+
+    int max_num=*a;
+
+    if(*b>max_num) max_num=*b;
+    if(*c>max_num) max_num=*c;
+    if(*d>max_num) max_num=*d;
+
+    return max_num;
+}
 
 int main(void){
 
@@ -13,13 +22,3 @@ int main(void){
     return 0;
 } 
 
-int max4(int *a,int *b,int *c,int *d){
-
-    int max_num=*a;
-
-    if(*b>max_num) max_num=*b;
-    if(*c>max_num) max_num=*c;
-    if(*d>max_num) max_num=*d;
-
-    return max_num;
-}

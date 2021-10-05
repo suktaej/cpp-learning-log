af8b90eece8f8020478416273c4be924165d3893
SeokTae Jeong <suktaej@gmail.com>
2021-10-05 09:33:53 +0000

    renamed

---
+++ cpp/001_dynamic_mem.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+void A_Fun(int &rPara);
+void B_Fun(int *pPara);
+void C_Fun(int &a, int &b);
+
+int main(void){
+
+	int *ninit1 = new int;
+	int *ninit2 = new int(10);
+	int sinit1(15);
+	
+	int &ref = sinit1;
+	int *ptrData = &sinit1;
+
+	*ninit1 = 5;
+
+	std::cout<<"N_ini1:"<<*ninit1<<std::endl; //5
+	std::cout<<"N_ini2:"<<*ninit2<<std::endl; //10
+
+	ref=20;
+	std::cout<<"S_ini1:"<<sinit1<<std::endl; //20
+
+	*ptrData = 30;
+	std::cout<<"S_ini1:"<<sinit1<<std::endl; //30
+
+	A_Fun(sinit1);
+	std::cout<<"S_ini1:"<<sinit1<<std::endl; //100
+	
+	B_Fun(&sinit1);
+	std::cout<<"S_ini1:"<<sinit1<<std::endl; //55
+	
+	B_Fun(ninit1);
+	std::cout<<"N_ini1:"<<*ninit1<<std::endl;
+
+	C_Fun(sinit1,*ninit1);
+	std::cout<<"S1,N1:"<<sinit1<<','<<*ninit2<<std::endl;
+
+	delete ninit1;
+	delete ninit2;
+	
+	return 0;
+}
+
+void A_Fun(int &rPara){
+
+	rPara=100;
+}
+
+void B_Fun(int *pPara){
+
+	int tmp(55);
+
+	//std::cout<<*pPara<<std::endl;
+	*pPara=tmp;
+}
+
+void C_Fun(int &a, int &b){
+
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+

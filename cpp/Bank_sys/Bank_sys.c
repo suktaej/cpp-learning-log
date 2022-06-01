25a49fa7d2459a9cf3df08a05cbad36d24f87621
SeokTae Jeong <suktaej@gmail.com>
2022-06-01 09:32:20 +0000

    Bank sys for C complete

--- cpp/Bank_sys/Bank_sys.c
+++ cpp/Bank_sys/Bank_sys.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
 
 #define NAME_LEN 16
 #define ACC_LEN 256
@@ -9,8 +11,11 @@ void MakeAccount(void);
 void DepositMoney(void);
 void WithdrawMoney(void);
 void ShowInfo(void);
+void ShowAll(void);
 
-enum {MAKE=1, DEPOSIT, WITHDRAW, INQUIRE, EXIT};
+int DigitCheck(char*);
+
+enum {MAKE=1, DEPOSIT, WITHDRAW, INQUIRE, ALLVIEW, EXIT};
 
 typedef struct
 {
@@ -25,12 +30,24 @@ int Accnum=0;
 int main(void)
 {
 	int choice;
+	char* choice_str;
+
+	choice_str=malloc(sizeof(char));
 
 	while(1)
 	{
 		ShowMenu();
 		printf("Please choose number>");
-		scanf("%d",&choice);
+		scanf("%s",choice_str);
+		while(getchar()!='\n'){}
+
+		if(DigitCheck(choice_str))
+			choice=strtol(choice_str,NULL,10);
+		else
+		{
+			printf("***Illegal Selection***\n");
+			continue;
+		}
 		printf("\n");
 
 		switch(choice)
@@ -47,6 +64,9 @@ int main(void)
 			case INQUIRE:
 				ShowInfo();
 				break;
+			case ALLVIEW:
+				ShowAll();
+				break;
 			case EXIT:
 				return 0;
 			default:
@@ -54,6 +74,7 @@ int main(void)
 		}
 	}
 
+	free(choice_str);
 	return 0;
 }
 
@@ -64,19 +85,37 @@ void ShowMenu(void)
 	printf("2.Deposit\n");
 	printf("3.Withdraw\n");
 	printf("4.Show State\n");
-	printf("5.Exit\n");
+	printf("5.Show All State\n");
+	printf("6.Exit\n");
 }
 
 void MakeAccount(void)
 {
 	int money;
+	char* money_str;
 	char name[NAME_LEN];
+
+	money_str=malloc(sizeof(char));
 
 	printf("[Make Account]\n");
 	printf("Name>");
 	scanf("%s",name);
-	printf("Deposit Amount>");
-	scanf("%d",&money);
+	while(getchar()!='\n'){}
+
+	while(1)
+	{
+		printf("Deposit Amount>");
+		scanf("%s",money_str);
+		while(getchar()!='\n'){}
+
+		if(DigitCheck(money_str))
+		{
+			money=strtol(money_str,NULL,10);
+			break;
+		}
+		else
+			printf("***Illegal Selection***\n");
+	}
 	printf("\n");
 
 	Accarr[Accnum].AccID=Accnum;
@@ -88,62 +127,126 @@ void MakeAccount(void)
 	printf("Balance:%d\n",Accarr[Accnum].Balance);
 
 	Accnum++;
+	free(money_str);
 }
 
 void DepositMoney(void)
 {
 	int money;
 	int id;
+	char* money_str;
+	char* id_str;
+
+	money_str=malloc(sizeof(char));
+	id_str=malloc(sizeof(char));
+
+	printf("[Deposit Money]\n");
 
 	while(1)
 	{
-		printf("[Deposit Money]\n");
-		printf("ID>");
-		scanf("%d",&id);
+		while(1)
+		{
+			printf("ID>");
+			scanf("%s",id_str);
+			while(getchar()!='\n'){}
+
+			if(DigitCheck(id_str))
+			{
+				id=strtol(id_str,NULL,10);
+				break;
+			}
+			else
+				printf("***Illegal Selection***\n");
+		}
 
 		if(id<Accnum&&id>=0)
 		{
-			printf("Deposit Amount>");
-			scanf("%d",&money);
-		
+			while(1)
+			{
+				printf("Money Amount>");
+				scanf("%s",money_str);
+				while(getchar()!='\n'){}
+
+				if(DigitCheck(money_str))
+				{
+					money=strtol(money_str,NULL,10);
+					break;
+				}
+				else
+					printf("***Illegal Selection***\n");
+			}
+
 			Accarr[id].Balance+=money;
 			printf("Deposit Complete\nCurrent Your balance:%d\n",Accarr[id].Balance);
 			return;
 		}
 		else 
-			printf("Illegal ID\n");
+			printf("***Illegal ID***\n");
 	}
+	free(id_str);
+	free(money_str);
 }
 
 void WithdrawMoney(void)
 {
-	int money;
-	int id;
+	int id,money;
 
+	char* money_str;
+	char* id_str;
+
+	money_str=malloc(sizeof(char));
+	id_str=malloc(sizeof(char));
+
+	printf("[Withdraw Money]\n");
+	
 	while(1)
 	{
-		printf("[Withdraw Money]\n");
-		printf("ID>");
-		scanf("%d",&id);
+		while(1)
+		{
+			printf("ID>");
+			scanf("%s",id_str);
+			while(getchar()!='\n'){}
+
+			if(DigitCheck(id_str))
+			{
+				id=strtol(id_str,NULL,10);
+				break;
+			}
+			else
+				printf("***Illegal Selection***\n");
+		}
 
 		if(id<Accnum&&id>=0)
 		{
-			printf("Withdrawal Amount>");
-			scanf("%d",&money);
-			
+			while(1)
+			{
+				printf("Withdrawal Amount>");
+				scanf("%s",money_str);
+				while(getchar()!='\n'){}
+				
+				if(DigitCheck(money_str))
+				{
+					money=strtol(money_str,NULL,10);
+					break;
+				}
+				else
+					printf("***Illegal Selection***\n");
+			}
+
 			if(Accarr[id].Balance<money)
 				printf("A shortage of balance\n");
 			else
 			{
-
 				Accarr[id].Balance-=money;
 				printf("Withdrawal Complete\nCurrent Your balance:%d\n",Accarr[id].Balance);
 				return;
 			}
 		}
 		else
-			printf("Illegal ID\n");
+			printf("***Illegal ID***\n");
 	}
+	free(money_str);
+	free(id_str);
 }
 
 void ShowInfo(void)
@@ -155,6 +258,7 @@ void ShowInfo(void)
 		printf("[INFO]\n");
 		printf("ID>");
 		scanf("%d",&id);
+		while(getchar()!='\n'){}
 
 		if(id<Accnum&&id>=0)
 		{
@@ -166,4 +270,30 @@ void ShowInfo(void)
 		else
 			printf("Illegal ID\n");
 	}
+}
+
+void ShowAll(void)
+{
+	int id;
+	
+	for(id=0;id<Accnum;id++)
+	{
+		printf("----------------------\n");
+		printf("ID:%d\n",Accarr[id].AccID);
+		printf("Name:%s\n",Accarr[id].Customer_Name);
+		printf("Balance:%d\n",Accarr[id].Balance);
+		printf("----------------------\n");
+	}
+}
+
+int DigitCheck(char* str)
+{
+	int i;
+
+	for(i=0;i<strlen(str);i++)
+	{
+		if(isdigit(str[i])==0)
+			return 0;
+	}
+	return 1;
 }

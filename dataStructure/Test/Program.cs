caa082c762eea26eb1a49a17921cff05912f4ccc
SeokTae Jeong <suktaej@gmail.com>
2023-09-13 22:26:15 +0900

    delete and pull

---
+++ dataStructure/Test/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Alg
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.CursorVisible=false;
+			
+			int lastTick =0;
+
+			while(true)
+			{
+				int currentTick = System.Enviroment.TickCount;
+				Console.SetCursorPosition(0,0);
+			}
+		}
+	}
+}

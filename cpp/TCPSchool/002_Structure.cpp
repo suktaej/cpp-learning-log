b9532261df099ee9bbbe5bc20dfd8a3262d4d463
suktaej <suktaej@gmail.com>
2023-10-12 17:57:55 +0900

    Structure

---
+++ cpp/TCPSchool/002_Structure.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+
+struct Name
+{
+    string first;
+    string last;
+};
+
+struct book
+{
+    string title;
+    //string author;
+    Name author;
+    int price;
+    int sale;
+} Book;
+
+int Calc(int, int);
+int PtrCalc(const book*);
+
+int main(void)
+{
+    book webBook = {"HTML&CSS",{"Kim","NARA"},28000,10000};
+    book javaBook = {"Java",{"Lee","DARE"},10000,5000};
+    int salePrice;
+
+    salePrice = Calc(webBook.price,webBook.sale);
+    cout<<webBook.author.first<<webBook.author.last<<"\\"<<webBook.title<<"\\"<<webBook.price<<"\\"<<salePrice<<endl;
+    
+    salePrice = PtrCalc(&javaBook);
+    cout<<javaBook.author.first<<javaBook.author.last<<"\\"<<javaBook.title<<"\\"<<javaBook.price<<"\\"<<salePrice<<endl;
+
+    return 0;
+}
+
+int Calc(int a, int b)
+{
+    return (a-b);
+}
+
+int PtrCalc(const book* wbook)
+{
+    //wbook->price=30000;
+    return(wbook->price - wbook->sale);
+}
